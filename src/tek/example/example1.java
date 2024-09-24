11ba62c88473d8725480c51ec1dc58b99ee8248c
test@test.com <test@test.com>
2024-09-24 13:53:52 -0700

    adding new line of code

--- src/tek/example/example1.java
+++ src/tek/example/example1.java
@@ -6,5 +6,8 @@ public class example1 {
   public static void main(String[] args) {
     int age = 20;
     System.out.println(age);
+
+    String name = "Saqib";
+    System.out.println(name);
   }
 }
